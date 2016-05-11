72527dc9a83581ee34111b3f9193bec483cd121a
Joel Egger <joel.egger@winarco.ch>
2016-05-11 11:01:43 +0200

    added fibonacci function

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
-	for ($i = 0; $i < 10; ++$i) {
-		echo 'Hello World';
+	echo 'Hello World';
+	
+	function fibonacci($index) {
+		if ($index < 0) {
+			return -1;
+		} else if ($index < 2) {
+			return 1;
+		} else {
+			return fibonacci($index - 1) + fibonacci($index - 2);
+		}
 	}

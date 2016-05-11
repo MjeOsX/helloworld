6156f31fd95fdf993ff037143912ed919d5da851
Joel Egger <joel.egger@winarco.ch>
2016-05-11 11:06:13 +0200

    fibonacci function improved

--- index.php
+++ index.php
@@ -4,9 +4,22 @@
 	function fibonacci($index) {
 		if ($index < 0) {
 			return -1;
-		} else if ($index < 2) {
-			return 1;
 		} else {
-			return fibonacci($index - 1) + fibonacci($index - 2);
+			$data = fibonacciArray($index);
+			return $data[0];
 		}
 	}
+	
+	function fibonacciArray($index) {
+		if ($index < 2) {
+			return array(1, 1);
+		} else {
+			$data = fibonacciArray($index - 1);
+			$temp = $data[1];
+			$data[1] = $data[0];
+			$data[0] += $temp;
+			return $data;
+		}
+	}
+	
+	echo fibonacci(10);
